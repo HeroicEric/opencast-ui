57264031bf8bbc05dcd30d68ee98e8e9b5b404a8
Eric Kelly <heroiceric@gmail.com>
2016-05-13 19:49:41 -0400

    wip

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,7 +4,9 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
-    // Add options here
+    fingerprint: {
+      prepend: 'https://d1711ybqwv7szk.cloudfront.net/opencast-ui/'
+    }
   });
 
   // Use `app.import` to add additional libraries to the generated
