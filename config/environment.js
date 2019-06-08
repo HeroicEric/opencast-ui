f0a5c16ef04aab76ca61ea301d3b0e18918d7208
Eric Kelly <heroiceric@gmail.com>
2019-06-08 10:49:29 -0700

    Use pods

--- config/environment.js
+++ config/environment.js
@@ -3,6 +3,7 @@
 module.exports = function(environment) {
   let ENV = {
     modulePrefix: 'opencast',
+    podModulePrefix: 'opencast/routes',
     environment,
     rootURL: '/',
     locationType: 'auto',
